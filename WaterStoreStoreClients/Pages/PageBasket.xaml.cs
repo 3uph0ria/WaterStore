3fbc98ef46c4fad4ece74c190adda1b622d6fce1
3uph0ria <46813688+3uph0ria@users.noreply.github.com>
2022-06-08 20:56:47 +0300

    Fitst commit

---
+++ WaterStoreStoreClients/Pages/PageBasket.xaml.cs
@@ -0,0 +1,154 @@
+﻿using WaterStore.Classes;
+using WaterStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WaterStore.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageBasket.xaml
+    /// </summary>
+    public partial class PageBasket : Page
+    {
+        public PageBasket()
+        {
+            InitializeComponent();
+            Update();
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavManager.MainFrame.Navigate(new Users());
+        }
+
+        private void AddBasket_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddOrder_Click(object sender, RoutedEventArgs e)
+        {
+
+            StringBuilder erros = new StringBuilder();
+
+            long n;
+            bool isNumeric = long.TryParse(Phone.Text, out n);
+
+            if (String.IsNullOrEmpty(FullName.Text))
+                erros.AppendLine("Введите ФИО");
+            else if (String.IsNullOrEmpty(Phone.Text))
+                erros.AppendLine("Введите телефон");
+            else if (isNumeric == false)
+                erros.AppendLine("В номере телефона должны быть только цифры");
+            else if(Phone.Text.Length < 11)
+                erros.AppendLine("В номере доллжно быть больше 11 символов");
+
+            if (erros.Length > 0)
+            {
+                MessageBox.Show(erros.ToString());
+                return;
+            }
+
+            MessageBox.Show(FullName.Text + ", Ваш заказ успешно сформирован, скоро Вам перезвонит администратор");
+
+            var clients = WaterStoreEntities.GetContext().Clients.ToList();
+            clients = clients.Where(p => p.Fullname.Contains(FullName.Text)).ToList();
+            var client = clients.FirstOrDefault();
+
+            if (client == null)
+            {
+                Clients currnetClient = new Clients();
+                currnetClient.Fullname = FullName.Text;
+                currnetClient.Phone = Phone.Text;
+                currnetClient.Adress = Adress.Text;
+                WaterStoreEntities.GetContext().Clients.Add(currnetClient);
+                WaterStoreEntities.GetContext().SaveChanges();
+                var newClients = WaterStoreEntities.GetContext().Clients.ToList();
+                var newClient = newClients.LastOrDefault();
+
+                foreach (var component in CurrentUser.currentServices)
+                {
+                    ClientService newSellService = new ClientService();
+                    newSellService.IdService = component.Id;
+                    newSellService.IdClient = newClient.Id;
+                    newSellService.Date = DateTime.Now;
+                    WaterStoreEntities.GetContext().ClientService.Add(newSellService);
+                    WaterStoreEntities.GetContext().SaveChanges();
+                }
+            }
+            else
+            {
+                foreach (var component in CurrentUser.currentServices)
+                {
+                    ClientService newSellService = new ClientService();
+                    newSellService.IdService = component.Id;
+                    newSellService.IdClient = client.Id;
+                    newSellService.Date = DateTime.Now;
+                    newSellService.Status = 1;
+                    WaterStoreEntities.GetContext().ClientService.Add(newSellService);
+                    WaterStoreEntities.GetContext().SaveChanges();
+                }
+            }
+        }
+
+        private void BtnDelService_Click(object sender, RoutedEventArgs e)
+        {
+            var service = (sender as Button).DataContext as Services;
+            CurrentUser.currentCost -= service.Cost;
+
+            var itemToRemove = CurrentUser.currentServices.Single(r => r.Id == service.Id);
+            CurrentUser.currentServices.Remove(itemToRemove);
+
+            NavManager.MainFrame.Navigate(new PageBasket());
+        }
+
+        public void Update()
+        {
+            ListServices.ItemsSource = CurrentUser.currentServices;
+            currentCost.Text = "Итого: " + CurrentUser.currentCost + "₽";
+        }
+
+
+        private void BtnAddQuantity_Click(object sender, RoutedEventArgs e)
+        {
+            var currentService = (sender as Button).DataContext as Services;
+
+            currentService.Quantity++;
+            CurrentUser.currentCost += currentService.Cost;
+            NavManager.MainFrame.Navigate(new PageBasket());
+        }
+
+        private void BtnDelQuantity_Click(object sender, RoutedEventArgs e)
+        {
+            var currentService = (sender as Button).DataContext as Services;
+
+            if (currentService.Quantity > 1)
+            {
+                currentService.Quantity--;
+                CurrentUser.currentCost -= currentService.Cost;
+                NavManager.MainFrame.Navigate(new PageBasket());
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void BtnBack_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavManager.MainFrame.Navigate(new Users());
+        }
+    }
+}
---
+++ WaterStoreStoreClients/Pages/Users.xaml.cs
@@ -0,0 +1,95 @@
+﻿using WaterStore.Classes;
+using WaterStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WaterStore.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Users.xaml
+    /// </summary>
+    public partial class Users : Page
+    {
+        public Users()
+        {
+
+            InitializeComponent();
+            Update();
+        }
+
+        private void Search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Update();
+        }
+
+        public void Update()
+        {
+            CountService.Badge = CurrentUser.currentServices.Count.ToString();
+
+            var services = WaterStoreEntities.GetContext().Services.ToList();
+
+          
+
+            services = services.Where(p => p.Name.ToLower().Contains(Search.Text.ToLower())).ToList();
+            ListServices.ItemsSource = services;
+            
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+           // NavManager.MainFrame.Navigate(new SignIn());
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Update();
+        }
+
+        private void AddBasket_Click(object sender, RoutedEventArgs e)
+        {
+            var service = (sender as Button).DataContext as Services;
+            CurrentUser.currentCost += service.Cost;
+
+            var currentServicesTmp = (sender as Button).DataContext as Services;
+            currentServicesTmp.Quantity = 1;
+            currentServicesTmp.Cost = currentServicesTmp.Cost;
+            CurrentUser.currentServices.Add(currentServicesTmp);
+            Update();
+            MessageBox.Show("Товар '" + service.Name + "' успешно добавлен к корзину!", "Внимание!", MessageBoxButton.OK);
+        }
+
+        private void BtnBack_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavManager.MainFrame.Navigate(new PageBasket());
+        }
+
+        private void BtnOrder_Click(object sender, RoutedEventArgs e)
+        {
+            var service = (sender as Button).DataContext as Services;
+            CurrentUser.currentCost += service.Cost;
+            CurrentUser.currentServices.Add((sender as Button).DataContext as Services);
+            NavManager.MainFrame.Navigate(new PageBasket());
+        }
+
+        private void BtnSelectCategory_Click(object sender, RoutedEventArgs e)
+        {
+            Categoris name = (Categoris)(sender as Button).DataContext as Categoris;
+            var services = WaterStoreEntities.GetContext().Services.ToList();
+            services = services.Where(p => p.Categoris.Name.Contains(name.Name)).ToList();
+            ListServices.ItemsSource = services;
+        }
+    }
+}
